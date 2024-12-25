d90b88ecf4e8396b91ad2ff686883c6cf525fcde
Mohammed Saleh <104917244+Mdsaleh99@users.noreply.github.com>
2024-12-25 22:15:17 +0530

    add test for hash password and unique email

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -3,16 +3,28 @@ import { User } from "../entity/User";
 import { userData } from "../types";
 import createHttpError from "http-errors";
 import { Roles } from "../constants";
+import bcrypt from "bcrypt";
 export class UserService {
     constructor(private userRepository: Repository<User>) {}
     async create({ firstName, lastName, email, password }: userData) {
+        const user = await this.userRepository.findOne({
+            where: { email: email },
+        });
+        if (user) {
+            const err = createHttpError(400, "Email is already exists!!");
+            throw err; // when error throws here. it goes to authController catch block and then error handled by global error handler which is in app.ts
+        }
+        // hash the password
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+
         // const userRepository = AppDataSource.getRepository(User)
         try {
             return await this.userRepository.save({
                 firstName,
                 lastName,
                 email,
-                password,
+                password: hashedPassword,
                 role: Roles.CUSTOMER,
             });
         } catch (err) {
